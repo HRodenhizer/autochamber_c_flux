349afe1b5b036d0e51a624a9e62eb9beebe18c34
HRodenhizer <heidi.rodenhizer@yahoo.com>
2021-07-06 16:24:28 -0700

    switches to using a conditional inference tree rather than a regression tree or gbm

--- CO2_flux_regression_tree_analysis.R
+++ CO2_flux_regression_tree_analysis.R
@@ -4,104 +4,128 @@
 #############################################################################################################################
 
 ### Load Libraries ##########################################################################################################
-library(gbm)
+# library(tree)
+library(partykit)
+library(data.table)
 library(tidyverse)
 #############################################################################################################################
 
 ### Load Data ###############################################################################################################
-flux.daily <- read.csv("/home/heidi/Documents/School/NAU/Schuur Lab/Autochamber/autochamber_c_flux_input_data/flux_daily_neat.csv")
-flux.weekly <- read.csv("/home/heidi/Documents/School/NAU/Schuur Lab/Autochamber/autochamber_c_flux_input_data/flux_weekly_neat.csv")
-flux.annual <- read.csv("/home/heidi/Documents/School/NAU/Schuur Lab/Autochamber/autochamber_c_flux_input_data/flux_annual.csv")
+flux.daily <- fread("/home/heidi/Documents/School/NAU/Schuur Lab/Autochamber/autochamber_c_flux/input_data/flux_daily_neat.csv")
+flux.weekly <- fread("/home/heidi/Documents/School/NAU/Schuur Lab/Autochamber/autochamber_c_flux/input_data/flux_weekly_neat.csv")
+flux.annual <- fread("/home/heidi/Documents/School/NAU/Schuur Lab/Autochamber/autochamber_c_flux/input_data/flux_annual.csv")
 #############################################################################################################################
 
 ### Gradient Boosted Regression Tree ########################################################################################
 set.seed(21591)
-train.weekly <- sample(1:nrow(flux.daily), 20000)
-train.annual <- sample(1:nrow(flux.daily), 1100)
-
-nee.daily <- flux.daily[!is.na(nee.sum), .SD, .SDcols = c(13, seq(16, 50))]
-gpp.daily <- flux.daily[!is.na(gpp.sum), .SD, .SDcols = c(15, seq(16, 50))]
-reco.daily <- flux.daily[!is.na(reco.sum), .SD, .SDcols = c(14, seq(16, 50))]
-train.nee.daily <- sample(1:nrow(nee.daily), 55000)
-train.gpp.daily <- sample(1:nrow(gpp.daily), 55000)
-train.reco.daily <- sample(1:nrow(reco.daily), 55000)
-
-nee.weekly <- flux.weekly[, .SD, .SDcols = c(12, seq(15, 135))][train.weekly,]
-gpp.weekly <- flux.weekly[, .SD, .SDcols = c(14, seq(15, 135))][train.weekly,]
-reco.weekly <- flux.weekly[, .SD, .SDcols = c(13, seq(15, 135))][train.weekly,]
-
-nee.annual <- flux.annual[, .SD, .SDcols = c(8, seq(11, 93))][train.annual,]
-gpp.annual <- flux.annual[, .SD, .SDcols = c(10, seq(11, 93))][train.annual,]
-reco.annual <- flux.annual[, .SD, .SDcols = c(9, seq(11, 93))][train.annual,]
-
-### Test out regression tree for stability (it would be nice to see the tree)
-library(tree)
-for (i in 1:20) {
-  set.seed(i)
-  train <- sample(1:nrow(nee.daily))
-  print(train[1:10])
-  test <- tree(nee.sum ~ .,
-               nee.daily,
-               subset = train)
-  plot(test)
-  text(test)
-}
 
 ### Daily
-### Can switch to using a regression tree!
+nee.daily <- flux.daily[!is.na(nee.sum), -c('date', 'year', 'flux.year',
+                                            'season', 'month', 'week', 'doy',
+                                            'block', 'fence', 'plot', 'plot.id',
+                                            'treatment', 'deployed', 'reco.sum',
+                                            'gpp.sum', 'alt.doy', 'ndvi.doy')]
+gpp.daily <- flux.daily[!is.na(gpp.sum),  -c('date', 'year', 'flux.year',
+                                             'season', 'month', 'week', 'doy',
+                                             'block', 'fence', 'plot', 'plot.id',
+                                             'treatment', 'deployed', 'reco.sum',
+                                             'nee.sum', 'alt.doy', 'ndvi.doy')]
+reco.daily <- flux.daily[!is.na(reco.sum),  -c('date', 'year', 'flux.year',
+                                               'season', 'month', 'week', 'doy',
+                                               'block', 'fence', 'plot', 'plot.id',
+                                               'treatment', 'deployed', 'nee.sum',
+                                               'gpp.sum', 'alt.doy', 'ndvi.doy')]
+train.nee.daily <- sample(1:nrow(nee.daily), 0.8*nrow(nee.daily))
+train.gpp.daily <- sample(1:nrow(gpp.daily), 0.8*nrow(gpp.daily))
+train.reco.daily <- sample(1:nrow(reco.daily), 0.8*nrow(reco.daily))
+
 # NEE 
-nee.daily.gbm <- gbm(nee.sum~.,
-                     data = nee.daily[train.nee.daily,],
-                     distribution = "gaussian",
-                     n.trees = 10000,
-                     shrinkage = 0.01,
-                     interaction.depth = 4)
-summary(nee.daily.gbm)
-nee.pred <- nee.daily[-train.nee.daily,
-                      nee.pred := predict(nee.daily.gbm,
-                                          newdata = nee.daily[-train.nee.daily,],
-                                          n.trees = 10000,
-                                          single.tree = TRUE)]
-ggplot(nee.pred, aes(x = nee.sum, y = nee.pred)) +
+nee.daily.tree <- ctree(nee.sum~.,
+                        data = nee.daily,
+                        subset = train.nee.daily) # this produces a huge tree... don't try to plot
+nee.daily.tree
+# plot(nee.daily.tree)
+# saveRDS(nee.daily.tree, '/home/heidi/Documents/School/NAU/Schuur Lab/Autochamber/autochamber_c_flux/model_output/nee_daily_tree.rds')
+nee.daily.fit <- nee.daily[-train.nee.daily, fit := predict(nee.daily.tree, nee.daily[-train.nee.daily,])]
+ggplot(nee.daily.fit, aes(x = fit, y = nee.sum)) +
   geom_point()
-plot(nee.daily.gbm, i = 't5.mean')
-pretty.gbm.tree(nee.daily.gbm, i.tree = 10000)
-# saveRDS(nee.daily.gbm, '/home/heidi/Documents/School/NAU/Schuur Lab/Autochamber/autochamber_c_flux/model_output/nee_daily_gbm.rds')
 
 # Reco
-reco.daily.gbm <- gbm(reco.sum~.,
-                     data = reco.daily[train.reco.daily,],
-                     distribution = "gaussian",
-                     n.trees = 10000,
-                     shrinkage = 0.01,
-                     interaction.depth = 4)
-summary(reco.daily.gbm)
-reco.pred <- reco.daily[-train.reco.daily,
-                      reco.pred := predict(reco.daily.gbm,
-                                          newdata = reco.daily[-train.reco.daily,],
-                                          n.trees = 10000,
-                                          single.tree = TRUE)]
-ggplot(reco.pred, aes(x = reco.sum, y = reco.pred)) +
+reco.daily.tree <- ctree(reco.sum~.,
+                        data = reco.daily,
+                        subset = train.reco.daily) # this produces a huge tree... don't try to plot
+reco.daily.tree
+# plot(reco.daily.tree)
+# saveRDS(reco.daily.tree, '/home/heidi/Documents/School/NAU/Schuur Lab/Autochamber/autochamber_c_flux/model_output/reco_daily_tree.rds')
+reco.daily.fit <- reco.daily[-train.reco.daily, fit := predict(reco.daily.tree, reco.daily[-train.reco.daily,])]
+ggplot(reco.daily.fit, aes(x = fit, y = reco.sum)) +
   geom_point()
-plot(reco.daily.gbm, i = 't10.min')
-pretty.gbm.tree(reco.daily.gbm, i.tree = 10000)
-# saveRDS(reco.daily.gbm, '/home/heidi/Documents/School/NAU/Schuur Lab/Autochamber/autochamber_c_flux/model_output/reco_daily_gbm.rds')
 
 # GPP
-gpp.daily.gbm <- gbm(gpp.sum~.,
-                      data = gpp.daily[train.gpp.daily,],
-                      distribution = "gaussian",
-                      n.trees = 10000,
-                      shrinkage = 0.01,
-                      interaction.depth = 4)
-summary(gpp.daily.gbm)
-gpp.pred <- gpp.daily[-train.gpp.daily,
-                        gpp.pred := predict(gpp.daily.gbm,
-                                             newdata = gpp.daily[-train.gpp.daily,],
-                                             n.trees = 10000,
-                                             single.tree = TRUE)]
-ggplot(gpp.pred, aes(x = gpp.sum, y = gpp.pred)) +
+gpp.daily.tree <- ctree(gpp.sum~.,
+                        data = gpp.daily,
+                        subset = train.gpp.daily) # this produces a huge tree... don't try to plot
+gpp.daily.tree
+# plot(gpp.daily.tree)
+# saveRDS(gpp.daily.tree, '/home/heidi/Documents/School/NAU/Schuur Lab/Autochamber/autochamber_c_flux/model_output/gpp_daily_tree.rds')
+gpp.daily.fit <- gpp.daily[-train.gpp.daily, fit := predict(gpp.daily.tree, gpp.daily[-train.gpp.daily,])]
+ggplot(gpp.daily.fit, aes(x = fit, y = gpp.sum)) +
   geom_point()
-plot(gpp.daily.gbm, i = 't10.min')
-pretty.gbm.tree(gpp.daily.gbm, i.tree = 10000)
-# saveRDS(gpp.daily.gbm, '/home/heidi/Documents/School/NAU/Schuur Lab/Autochamber/autochamber_c_flux/model_output/gpp_daily_gbm.rds')
+
+### Weekly
+# Should I limit the number of splits using a smaller alpha value?
+# Have to split the data set into training ahead of time, does not work with subset.
+# use:
+# control = ctree_control(alpha =10^-15)
+nee.weekly <- flux.weekly[!is.na(nee.sum), -c('year', 'flux.year',
+                                            'season', 'month', 'week',
+                                            'block', 'fence', 'plot', 'plot.id',
+                                            'treatment', 'reco.sum',
+                                            'gpp.sum', 'alt.doy', 'ndvi.doy')]
+gpp.weekly <- flux.weekly[!is.na(gpp.sum),  -c('year', 'flux.year',
+                                               'season', 'month', 'week',
+                                               'block', 'fence', 'plot', 'plot.id',
+                                               'treatment', 'reco.sum',
+                                               'nee.sum', 'alt.doy', 'ndvi.doy')]
+reco.weekly <- flux.weekly[!is.na(reco.sum),  -c('year', 'flux.year',
+                                                 'season', 'month', 'week',
+                                                 'block', 'fence', 'plot', 'plot.id',
+                                                 'treatment', 'nee.sum',
+                                                 'gpp.sum', 'alt.doy', 'ndvi.doy')]
+train.nee.weekly <- sample(1:nrow(nee.weekly), 0.8*nrow(nee.weekly))
+train.gpp.weekly <- sample(1:nrow(gpp.weekly), 0.8*nrow(gpp.weekly))
+train.reco.weekly <- sample(1:nrow(reco.weekly), 0.8*nrow(reco.weekly))
+
+# NEE 
+nee.weekly.tree <- ctree(nee.sum~.,
+                        data = nee.weekly,
+                        subset = train.nee.weekly) # this produces a huge tree... don't try to plot
+nee.weekly.tree
+# plot(nee.weekly.tree)
+# saveRDS(nee.weekly.tree, '/home/heidi/Documents/School/NAU/Schuur Lab/Autochamber/autochamber_c_flux/model_output/nee_weekly_tree.rds')
+nee.weekly.fit <- nee.weekly[-train.nee.weekly, fit := predict(nee.weekly.tree, nee.weekly[-train.nee.weekly,])]
+ggplot(nee.weekly.fit, aes(x = fit, y = nee.sum)) +
+  geom_point()
+
+# Reco
+reco.weekly.tree <- ctree(reco.sum~.,
+                         data = reco.weekly,
+                         subset = train.reco.weekly) # lowering alpha results in fewer splits
+reco.weekly.tree
+plot(reco.weekly.tree)
+# saveRDS(reco.weekly.tree, '/home/heidi/Documents/School/NAU/Schuur Lab/Autochamber/autochamber_c_flux/model_output/reco_weekly_tree.rds')
+reco.weekly.fit <- reco.weekly[-train.reco.weekly, fit := predict(reco.weekly.tree, reco.weekly[-train.reco.weekly,])]
+ggplot(reco.weekly.fit, aes(x = fit, y = reco.sum)) +
+  geom_point()
+
+# GPP
+gpp.weekly.tree <- ctree(gpp.sum~.,
+                        data = gpp.weekly,
+                        subset = train.gpp.weekly) # this produces a huge tree... don't try to plot
+gpp.weekly.tree
+# plot(gpp.weekly.tree)
+# saveRDS(gpp.weekly.tree, '/home/heidi/Documents/School/NAU/Schuur Lab/Autochamber/autochamber_c_flux/model_output/gpp_weekly_tree.rds')
+gpp.weekly.fit <- gpp.weekly[-train.gpp.weekly, fit := predict(gpp.weekly.tree, gpp.weekly[-train.gpp.weekly,])]
+ggplot(gpp.weekly.fit, aes(x = fit, y = gpp.sum)) +
+  geom_point()
+
